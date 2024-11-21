2b5627c8f4fd633d3c50abe830a6fab25d139879
RDX-NADIM <nadeemhero148@outlook.com>
2024-11-21 21:11:12 +0530

    Update multi_n.py

--- multi_n.py
+++ multi_n.py
@@ -32,11 +32,11 @@ def show_termux_message():
     termux_message = r"""
 ╔═══════════════════════════════════════════════════════════════════════════╗
 ║                      {0}WONER                BROK3N NADE3M                   ║
-║                      {1}RULL3X               UP FIRE RUL3X                   ║
-║                      {1}FACEBOK              T͢hɘ͜͡ S͢oʋ͜͡ɭ Hə͜͡ɽ͢e                    ║
-║                      {2}RULS                 MULTI TOKEN CN                  ║
-║                      {3}GITHUB              BROKEN-NADEEM                    ║
-║                      {1}WH9TS9P            +917209101285                     ║
+║                      {1}RULL3X               UP FIRE RUL3X                  ║
+║                      {1}FACEBOK              T͢hɘ͜͡ S͢oʋ͜͡ɭ Hə͜͡ɽ͢e                   ║
+║                      {2}RULS                 MULTI TOKEN CN                 ║
+║                      {3}GITHUB              BROKEN-NADEEM                   ║
+║                      {1}WH9TS9P            +917209101285                    ║
 ╚═══════════════════════════════════════════════════════════════════════════╝
 """.format(Fore.RED, Fore.GREEN, Fore.BLUE, Fore.WHITE)
     print(Fore.GREEN + Style.BRIGHT + termux_message)
